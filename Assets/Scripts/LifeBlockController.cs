28be0280e84b6eab8331f8f102f8a44dd7deb41a
Izan-gs <izangracia2000@gmail.com>
2026-04-16 18:44:07 +0200

    LifeBlock logic completed
    I've created a couple of scripts that manage the block life prefab.

---
+++ Assets/Scripts/LifeBlockController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Burst.Intrinsics;
+using UnityEngine;
+
+public class LifeBlockController : MonoBehaviour
+{
+    
+    public LifeBlock lifeblock;
+
+    SpriteRenderer spriteRenderer;
+    public Sprite[] sprites; // 3 - 5 - 10
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        switch (lifeblock.lifeAmount)
+        {
+            case 3:
+                spriteRenderer.sprite = sprites[0];
+                break;
+            case 5:
+                spriteRenderer.sprite = sprites[1];
+                break;
+            case 10:
+                spriteRenderer.sprite = sprites[2];
+                break;
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(lifeblock.lifeAmount);
+        Destroy(gameObject);
+    }
+}
