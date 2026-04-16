4a30a7ab700b20d1974c3824acdaa1187f37e830
Izan-gs <izangracia2000@gmail.com>
2026-04-16 18:08:34 +0200

    LifeBlock code

---
+++ Assets/Scripts/ScriptableObjects/LifeBlock.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LifeBlock", menuName = "Scriptable Objects/LifeBlock")]
+public class LifeBlock : ScriptableObject
+{
+    public float lifeAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
