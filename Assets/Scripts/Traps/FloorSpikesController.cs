efbf4359eb2458980c701fa646d4ae462f400d5c
Izan-gs <izangracia2000@gmail.com>
2026-04-21 17:18:45 +0200

    Improved collision in basic block
    Improved collision and created spikes logic

--- Assets/Scripts/Traps/FloorSpikesController.cs
+++ Assets/Scripts/Traps/FloorSpikesController.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class FloorSpikesController : MonoBehaviour
 {
+    [SerializeField] private float minFallSpeedToDie = 0.1f;
+
     // Detects if player collides from above and then player dies
     private void OnCollisionEnter2D(Collision2D collision)
     {
         PlayerController player = collision.gameObject.GetComponentInParent<PlayerController>();
+        if (player == null) return;
 
-        if (player != null)
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+        if (rb == null) return;
+
+        // Obtenemos SOLO el primer contacto
+        ContactPoint2D contact = collision.GetContact(0);
+
+        // Condiciones tipo Mortol
+        bool hitFromAbove = contact.normal.y > 0.5f;
+        bool isFalling = rb.velocity.y < -minFallSpeedToDie;
+
+        if (hitFromAbove && isFalling)
         {
-            player.Die();
+            KillPlayer(player);
         }
+    }
+
+    private void KillPlayer(PlayerController player)
+    {
+        Debug.Log("Jugador muerto por pinchos");
+        player.Die();
     }
 }
